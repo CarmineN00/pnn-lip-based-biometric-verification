8be18b5e1ac017ecddb415ee6303e794fe4373af
DarioTroll <puddino1337@gmail.com>
2023-04-30 12:43:25 +0200

    Dataset generator script, bug fixing

--- read_data.py
+++ read_data.py
@@ -4,6 +4,8 @@ import numpy as np
 import os, glob, cv2
 import mediapipe as mp
 import LipLandmarks as lp
+import csv
+from tqdm import tqdm
 
 
 def ottieni_lista_distanze_euclidee(filename):
@@ -36,39 +38,62 @@ def ottieni_lista_distanze_euclidee(filename):
         for j in range(0, len(lp.lip_landmarks)):
             i = next(iterator)
 
-            # first element of tuple
-            point = results.multi_face_landmarks[0].landmark[i[0]]
-            node1_x = int(point.x * width)
-            node1_y = int(point.y * height)
-            landmarks.append((node1_x, node1_y))
-            cv2.circle(frame, (node1_x, node1_y), 2, (255, 255, 255), -1)
+            '''
+                Non è detto che da un video sia sempre possibile 
+                fare detection di almeno un volto, quindi questo 
+                controllo è strettamente necessario
+            '''
+            if results and results.multi_face_landmarks:
+                # Primo elemento della tupla
+                point = results.multi_face_landmarks[0].landmark[i[0]]
+                node1_x = int(point.x * width)
+                node1_y = int(point.y * height)
+                landmarks.append((node1_x, node1_y))
+                cv2.circle(frame, (node1_x, node1_y), 2, (255, 255, 255), -1)
 
-            # second element of tuple
-            point = results.multi_face_landmarks[0].landmark[i[1]]
-            node2_x = int(point.x * width)
-            node2_y = int(point.y * height)
-            landmarks.append((node2_x, node2_y))
-            cv2.circle(frame, (node2_x, node2_y), 2, (255, 255, 255), -1)
+                # Secondo elemento della tupla
+                point = results.multi_face_landmarks[0].landmark[i[1]]
+                node2_x = int(point.x * width)
+                node2_y = int(point.y * height)
+                landmarks.append((node2_x, node2_y))
+                cv2.circle(frame, (node2_x, node2_y), 2, (255, 255, 255), -1)
 
-            # Calcolo della distanza euclidea tra i punti
-            d = math.sqrt((node2_x - node1_x) ** 2 + (node2_y - node1_y) ** 2)
-            list_of_euclidean_distances.append(d)
+                # Calcolo della distanza euclidea tra i punti
+                d = math.sqrt((node2_x - node1_x) ** 2 + (node2_y - node1_y) ** 2)
+                list_of_euclidean_distances.append(d)
 
     return list_of_euclidean_distances
 
+
 def main():
+    with open('dataset.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
 
-    os.chdir("Dataset")
-    video_dir = os.listdir()
+        header_string = []
+        for i in range(20):
+            header_string.append("Feature "+str(i))
+        header_string.append("Label")
+        writer.writerow(header_string)
 
-    for dir in video_dir:
-        files = glob.glob(dir + "/*.avi")
-        for video in files:
-            res = ottieni_lista_distanze_euclidee(video)
-            print("Nome del video: ", video)
-            print(np.shape(res))
-            print(res)
-            print("\n\n")
-            
+        os.chdir("Dataset")
+        video_dir = os.listdir()
+
+        for directory in video_dir:
+            if os.path.isdir(directory):
+                files = glob.glob(directory + "/*.avi")
+
+                # Progress bar
+                for video in tqdm(files, desc=directory, ncols=100):
+                    res = ottieni_lista_distanze_euclidee(video)
+                    # Se necessario, cambiare qui il metodo di prelievo della label
+                    video_label = int(''.join(video.split("\\")[1].split(".")[0].split("_")[:4]))
+                    # print(video_label)
+                    if np.shape(res)[0] == 60:
+                        res_split = np.array_split(res, 3)
+                        for i in range(3):
+                            info_row = res_split[i]
+                            writer.writerow(np.append(info_row, video_label))
+
+
 if __name__ == "__main__":
     main()
