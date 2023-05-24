821491a6b11ec7bbc8b2423f1176d7c8eb850f45
CarmineNapolitano <carmine-2000@outlook.it>
2023-05-24 17:12:13 +0200

    Added delaunay config and a printer for delaunay

--- print_landmarks.py
+++ print_landmarks.py
@@ -1,8 +1,14 @@
 import cv2
 import mediapipe as mp
 import LipLandmarks as lp
+from scipy.spatial import Delaunay
+import numpy as np
+import math 
 
-def p_landmarks(image_path):
+
+def p_landmarks(image_path,type):
+    landmarks = []
+
     mp_face_mesh = mp.solutions.face_mesh
     face_mesh = mp_face_mesh.FaceMesh()
 
@@ -20,16 +26,64 @@ def p_landmarks(image_path):
             point1 = results.multi_face_landmarks[0].landmark[i[0]]
             node1_x = int(point1.x * width)
             node1_y = int(point1.y * height)
-            cv2.circle(image, (node1_x, node1_y), 1, (255, 255, 255), -1)
+            if(type=="delaunay"):
+                landmarks.append((node1_x,node1_y))
+            elif(type=="standard"):
+                cv2.circle(image, (node1_x, node1_y), 2, (255, 0, 0), -1)
             # Secondo elemento della tupla
             point2 = results.multi_face_landmarks[0].landmark[i[1]]
             node2_x = int(point2.x * width)
             node2_y = int(point2.y * height)
-            cv2.circle(image, (node2_x, node2_y), 1, (255, 255, 255), -1)
+            if(type=="delaunay"):
+                landmarks.append((node2_x,node2_y))
+            elif(type=="standard"):
+                cv2.circle(image, (node2_x, node2_y), 2, (255, 0, 0), -1)
+                cv2.line(image, (node1_x, node1_y), (node2_x,node2_y), (255, 255, 255), 1)
 
-    cv2.imwrite("landmarks_image.jpg", image)
+    if(type=="standard"):
+        cv2.imwrite("standard_landmarks.jpg", image)
+    elif(type=="delaunay"):
+        landmarks = set(landmarks)
+        landmarks = list(landmarks)
 
+        landmarks_np = np.array(landmarks)
+        triangles = Delaunay(landmarks_np)
+        distances =[]
+
+        # Set per tenere traccia dei segmenti unici
+        unique_segments = set()
+
+        # Lista per salvare le distanze euclidee
+        distances = []
+
+        # Itera sui triangoli
+        for tri in triangles.simplices:
+            pt1, pt2, pt3 = landmarks_np[tri]
+            
+            # Controllo per segmenti unici
+            segment1 = tuple(sorted([tuple(pt1), tuple(pt2)]))
+            segment2 = tuple(sorted([tuple(pt2), tuple(pt3)]))
+            segment3 = tuple(sorted([tuple(pt3), tuple(pt1)]))
+            
+            # Disegna solo segmenti unici
+            if segment1 not in unique_segments:
+                cv2.circle(image,(pt1[0],pt1[1]), 2, (255, 0, 0), -1)
+                cv2.line(image, tuple(pt1), tuple(pt2), (255, 255, 255), 1)
+            if segment2 not in unique_segments:
+                cv2.circle(image,(pt2[0],pt2[1]), 2, (255, 0, 0), -1)
+                cv2.line(image, tuple(pt2), tuple(pt3), (255, 255, 255), 1)
+            if segment3 not in unique_segments:
+                cv2.circle(image,(pt3[0],pt3[1]), 2, (255, 0, 0), -1)
+                cv2.line(image, tuple(pt3), tuple(pt1), (255, 255, 255), 1)
+
+        cv2.imwrite("delaunay_landmarks.jpg", image)
+        # Stampa il numero di distanze euclidee ottenute (41)
+        # print("Distanze euclidee:")
+        # print(len(distances))
+        
 
 if __name__ == "__main__":
     path = "assets\Mike.jpg"
-    p_landmarks(path)
+
+    # il secondo paramentro deve essere : "standard" o "delaunay"
+    p_landmarks(path,"delaunay")
--- read_data.py
+++ read_data.py
@@ -9,6 +9,7 @@ import csv
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import scale
 from tqdm import tqdm
+from scipy.spatial import Delaunay
 
 def create_hashmap_from_ugly_labels_to_numbers(list):
     hashmap = {}
@@ -158,13 +159,99 @@ def ottieni_features_da_video(filename,type, num_frames):
 
     return list_of_euclidean_distances
 
-def create_csv(csv_filename, directory, type, num_frames):
+def ottieni_features_delaunay(filename, num_frames):
+    mp_face_mesh = mp.solutions.face_mesh
+    face_mesh = mp_face_mesh.FaceMesh()
+
+    cap = cv2.VideoCapture(filename)
+
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.set(cv2.CAP_PROP_POS_FRAMES, total_frames - num_frames - 1)
+
+    ret, frame = cap.read()
+
+    landmarks = []
+    list_of_euclidean_distances = []
+    lower_bound = 29
+
+    while ret:
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        height, width, _ = frame.shape
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = face_mesh.process(rgb_frame)
+
+        iterator = iter(lp.lip_landmarks)
+        for j in range(0, len(lp.lip_landmarks)):
+            i = next(iterator)
+
+            if results and results.multi_face_landmarks:
+                # Primo elemento della tupla
+                point1 = results.multi_face_landmarks[0].landmark[i[0]]
+                node1_x = int(point1.x * width)
+                node1_y = int(point1.y * height)
+                landmarks.append((node1_x,node1_y))
+
+                # Secondo elemento della tupla
+                point2 = results.multi_face_landmarks[0].landmark[i[1]]
+                node2_x = int(point2.x * width)
+                node2_y = int(point2.y * height)
+                landmarks.append((node2_x,node2_y))
+
+        landmarks_np = np.array(landmarks)
+        landmarks_np = np.unique(landmarks_np, axis=0)
+        print(np.shape(landmarks_np))
+        landmarks_np = np.reshape(landmarks_np, (len(landmarks_np), 2))
+        triangles = Delaunay(landmarks_np)
+
+        # Set per tenere traccia dei segmenti unici
+        unique_segments = set()
+
+        # Itera sui triangoli
+        for tri in triangles.simplices:
+            pt1, pt2, pt3 = landmarks_np[tri]
+            
+            # Controllo per segmenti unici
+            segment1 = tuple(sorted([tuple(pt1), tuple(pt2)]))
+            segment2 = tuple(sorted([tuple(pt2), tuple(pt3)]))
+            segment3 = tuple(sorted([tuple(pt3), tuple(pt1)]))
+            
+            # Disegna solo segmenti unici
+            if segment1 not in unique_segments:
+                unique_segments.add(segment1)
+                dist = np.linalg.norm(pt2 - pt1)
+                list_of_euclidean_distances.append(dist)
+            if segment2 not in unique_segments:
+                unique_segments.add(segment2)
+                dist = np.linalg.norm(pt3 - pt2)
+                list_of_euclidean_distances.append(dist)
+            if segment3 not in unique_segments:
+                unique_segments.add(segment3)
+                dist = np.linalg.norm(pt3 - pt1)
+                list_of_euclidean_distances.append(dist)
+
+            if len(list_of_euclidean_distances) >= lower_bound:
+                return list_of_euclidean_distances
+        
+        return list_of_euclidean_distances
+
+def create_csv(csv_filename, directory, type, num_frames, experiment):
     with open(csv_filename, mode='w', newline='') as file:
         writer = csv.writer(file)
 
         header_string = []
 
-        for i in range(20):
+        num_features = 0
+        if experiment == "delaunay":
+            num_features = 41
+        else:
+            num_features = 20
+        
+        for i in range(num_features):
             header_string.append("Feature " + str(i))
 
         header_string.append("Label")
@@ -174,10 +261,16 @@ def create_csv(csv_filename, directory, type, num_frames):
             files = glob.glob(directory + "/*.avi")
 
             for video in tqdm(files, desc=directory, ncols=100):
-                res = ottieni_features_da_video(video,type, num_frames)
+                res = []
+                if experiment == "delaunay":
+                    res = ottieni_features_delaunay(video, num_frames)
+                else:
+                    res = ottieni_features_da_video(video,type, num_frames)
                 video_label = str(''.join(video.split("\\")[1].split(".")[0].split("_")[:4]))
 
-                if np.shape(res)[0] == 20 * num_frames:
+                print("print shape of res: ",np.shape(res))
+
+                if np.shape(res)[0] == num_features * num_frames:
                     res_split = np.array_split(res, num_frames)
 
                     for i in range(num_frames):
@@ -187,5 +280,7 @@ def create_csv(csv_filename, directory, type, num_frames):
                     print("Failed to fetch lip features from video: ",video)'''
 
 if __name__ == "__main__":
-    create_data_correctly("train_dataset.csv", "test_dataset.csv")
+    #create_data_correctly("train_dataset.csv", "test_dataset.csv")
+    create_csv("delaunay_test.csv","Dataset/Test","",20,"delaunay")
+
 
